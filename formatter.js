cc172bdf2401151ca47aaab3a6caf82037fe2441
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-02-18 13:23:10 +0200

    added format from file

--- formatter.js
+++ formatter.js
@@ -1,52 +1,84 @@
 'use strict';
 
-/* Usage:
+/* Usage
  * First run installed() to check whether clang-format is installed and usable.
  * If everything is fine, the function should return true.
  *
- * To get formatted code, call getFormattedCode() with the unformatted code and wait until it resolves.
+ * To get formatted code, call format() with the unformatted code and wait until it resolves.
+ * To format a file, call formatFromFile() and wait until it resolves.
+ *
+ * Both functions reject if clang-format fails.
  */
 
 
 const childProcess = require('child_process');
+const path = require('path');
+
+let formatterInstalled = null;
 
 
-function installed() {
-	const formatProcess = childProcess.exec('clang-format', err => {
-		if (err) {
-			console.error('Clang-format is not installed, exiting...');
-			return false;
-		}
+function init() {
+	return new Promise(resolve => {
+		const formatProcess = childProcess.exec('clang-format', err => {
+			resolve(formatterInstalled = !err);
+		});
+
+		formatProcess.stdin.end();
 	});
-
-	formatProcess.stdin.end();
-	return true;
 }
 
 
-function getFormattedCode(code) {
+function installed() {
+	return new Promise(resolve => {
+		if (formatterInstalled === null) {
+			init().then(res => resolve(res));
+		} else {
+			resolve(formatterInstalled);
+		}
+	});
+}
+
+
+function format(code) {
 	return new Promise((resolve, reject) => {
 		const formatProcess = childProcess.exec('clang-format');
 		formatProcess.stdin.write(code);
 		formatProcess.stdin.end();
 
-		let formattedCode = '';
-		formatProcess.stdout.on('data', chunk => {
-			formattedCode += chunk;
-		});
+		handleProcessOutputAndExit(formatProcess, resolve, reject);
+	});
+}
 
-		formatProcess.on('close', (exitCode, signal) => {
-			if (!exitCode) {
-				resolve(formattedCode);
-			} else {
-				reject(signal);
-			}
-		})
+
+function formatFile(filePath) {
+	return new Promise((resolve, reject) => {
+		const formatProcess = childProcess.exec('clang-format ' +
+			path.resolve(__dirname, filePath));
+
+		handleProcessOutputAndExit(formatProcess, resolve, reject);
+	});
+}
+
+
+function handleProcessOutputAndExit(formatProcess, resolve, reject) {
+	let formattedCode = '';
+	let err = '';
+
+	formatProcess.stdout.on('data', chunk => formattedCode += chunk);
+	formatProcess.stderr.on('data', chunk => err += chunk);
+
+	formatProcess.on('close', exitCode => {
+		if (!exitCode) {
+			resolve(formattedCode);
+		} else {
+			reject(err);
+		}
 	});
 }
 
 
 module.exports = {
 	installed,
-	getFormattedCode
+	format,
+	formatFile
 };
--- syntax-checker.js
+++ syntax-checker.js
@@ -2,15 +2,37 @@
 
 // clang -fsyntax-only -x c++ -
 
+const childProcess = require('child_process');
+const path = require('path');
+
+let compilerInstalled = null;
+
+
+function init() {
+	return new Promise(resolve => {
+		const formatProcess = childProcess.exec('clang', err => {
+			resolve(compilerInstalled = !err);
+		});
+
+		formatProcess.stdin.end();
+	});
+}
+
 
 function installed() {
-
+	return new Promise(resolve => {
+		if (compilerInstalled === null) {
+			init().then(() => resolve(compilerInstalled));
+		} else {
+			resolve(compilerInstalled);
+		}
+	});
 }
 
 
 function checkCode(code) {
 	return new Promise((resolve, reject) => {
-		code? resolve() : reject();
+
 	});
 }
 
