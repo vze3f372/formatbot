5e88963d8e6639edfe340d6dcb48e98279cc5eef
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-02-24 10:50:01 +0200

    made some files optional
    code cleanup

--- codebot.js
+++ codebot.js
@@ -22,6 +22,7 @@ config.defaults = {
 config.load().then(config => {
 	client.login(config.token);
 
+
 	client.on('ready', () => {
 		console.info('Logged in as CodeBot!');
 		if (config.welcome.enabled) {
@@ -32,182 +33,196 @@ config.load().then(config => {
 		}
 	});
 
+
 	client.on('message', message => {
 		if (message.author.id === client.user.id) {
 			// Message sent by the bot, ignoring
 		} else if (message.content.startsWith('!codebot ')) {
-			const words = message.content.match(/\b\w+\b/g);
-			const commands = [
-				{
-					name: 'chadd',
-					admin: true,
-					cb: () => {
-						if (!config.channels.includes(message.channel.id)) {
-							config.channels.push(message.channel.id);
-							config.save();
-							message.reply('Channel added!');
-						}
-					}
-				},
-				{
-					name: 'chdel',
-					admin: true,
-					cb: () => {
-						config.channels.splice(config.channels.indexOf(message.channel.id));
-						config.save();
-						message.reply('Channel removed!');
-					}
-				},
-				{
-					name: 'chlist',
-					admin: true,
-					cb: () => {
-						message.reply('List of CodeBot channels:\n' +
-							config.channels.join('\n'));
-					}
-				},
-				{
-					name: 'promote',
-					admin: true,
-					cb: () => {
-						if (!config.admins.includes(words[2])) {
-							config.admins.push(words[2]);
-							config.save();
-							message.reply('User promoted!');
-						}
-					}
-				},
-				{
-					name: 'demote',
-					admin: true,
-					cb: () => {
-						config.admins.splice(config.admins.indexOf(words[2]));
-						config.save();
-						message.reply('User demoted!');
-					}
-				},
-				{
-					name: 'prset',
-					admin: true,
-					cb: () => {
-						const project = config.projects.find(p => p.name === words[2]);
-						for (const p of config.projects) {
-							if (p !== project) {
-								p.channels.splice(p.channels.indexOf(message.channel.id));
-							}
-						}
-						if (project) {
-							project.channels.push(message.channel.id);
-							message.reply('Project for this channel is set to ' + project.name + '!');
-						} else {
-							message.reply('Project not found, using empty project.');
-						}
-						config.save();
-					}
-				},
-				{
-					name: 'admins',
-					admin: true,
-					cb: () => {
-						message.reply('List of CodeBot admins:\n' +
-							config.admins.join('\n'));
-					}
-				},
-				{
-					name: 'version',
-					cb: () => {
-						configurer('./package.json').load().then(pkg => {
-							message.reply('CodeBot version: ' + pkg.version);
-						});
-					}
-				},
-				{
-					name: 'ahelp',
-					admin: true,
-					cb: () => {
-						message.reply('CodeBot admin help\n' +
-							'!codebot chadd - Adds the current channel to CodeBot\n' +
-							'!codebot chdel - Removes the current channel from CodeBot\n' +
-							'!codebot chlist - Lists channels added to CodeBot\n' +
-							'!codebot promote [id] - Sets user as admin\n' +
-							'!codebot demote [id] - Removes the user from admins\n' +
-							'!codebot admins - Lists all the admins of CodeBot' +
-							'!codebot prset - Sets the project to use for current channel\n' +
-							'!codebot ahelp - Shows this page\n' +
-							'!codebot help - Shows user help page');
-					}
-				},
-				{
-					name: 'help',
-					cb: () => {
-						message.reply('CodeBot help\n' +
-							'Just send me your code and I\'ll format it and check it for any errors!\n' +
-							'!codebot help - Shows this page\n' +
-							'!codebot ahelp - Shows admin help page\n' +
-							'!codebot version - Shows bot version');
-					}
-				}
-			];
-			const command = commands.find(c => c.name === words[1]);
-			if (!command) {
-				message.reply('Command not found');
-			} else if (command.admin && !config.admins.includes(message.author.id)) {
-				message.reply('This command requires admin permissions');
-			} else {
-				command.cb();
-			}
+			handleCommands(message);
 		} else if (!config.channels.includes(message.channel.id)) {
 			// Channel not added, ignoring
 		} else {
-			const attachments = message.attachments.array();
-			message.channel.send('Building, please wait...', attachments).then(reply => {
-				let promise = new Promise(resolve => resolve());
-				const project = config.projects.find(p => p.channels.includes(message.channel.id)) ??
-					config.projects.find(p => p.name === 'empty');
-
-				promise = promise
-					.then(() => fm.cleanDirectory(project.upload))
-					.then(() => fm.cleanDirectory(config.tempDir));
-
-				if (attachments.length) {
-					const sourceFile = attachments.find(a => a.url.match(/.*\.c(pp)?$/));
-					const sourceArchive = attachments.find(a => a.url.match(/.*\.zip$/));
-					if (sourceFile) {
-						promise = promise
-							.then(() => fm.downloadFile(sourceFile.url, project.upload))
-							.then(() => syntaxChecker.checkProject(project.root));
-					} else if (sourceArchive) {
-						promise = promise
-							.then(() => fm.cleanDirectory(project.upload))
-							.then(() => fm.downloadFile(sourceArchive.url, config.tempDir))
-							.then(() => fm.unArchive(config.tempDir + sourceArchive.name, project.upload))
-							.then(() => syntaxChecker.checkProject(project.root));
-					} else {
-						promise = new Promise((resolve, reject) => reject('File type not supported'));
-					}
-				} else {
-					promise = promise
-						.then(() => fm.saveToFile(project.upload + 'upload.cpp', message.content))
-						.then(() => syntaxChecker.checkProject(project.root));
-				}
-				const archivePath = config.tempDir + 'CodeBot.zip';
-				promise
-					.then(warnings => fm.saveToFile(project.upload + 'warnings.txt', warnings))
-					.catch(err => fm.saveToFile(project.upload + 'errors.txt', err))
-					.then(() => fm.saveToFile(project.upload + 'message.txt', message.content))
-					.then(() => fm.archive(project.upload, archivePath))
-					.then(() => message.channel.send('<@' + message.author.id + '>,\n' +
-						'See attached file for detailed output.',
-						new Discord.MessageAttachment(fs.createReadStream(archivePath))
-					))
+			message.channel.send('Building, please wait...').then(reply => {
+				processCode(message)
 					.finally(() => reply.delete());
-			})
-				.finally(() => message.delete());
+			}).finally(() => message.delete());
 		}
 	});
+
+
+	function handleCommands(message) {
+		const words = message.content.match(/\b\w+\b/g);
+		const commands = [
+			{
+				name: 'chadd',
+				admin: true,
+				cb: () => {
+					if (!config.channels.includes(message.channel.id)) {
+						config.channels.push(message.channel.id);
+						config.save();
+						message.reply('Channel added!');
+					}
+				}
+			},
+			{
+				name: 'chdel',
+				admin: true,
+				cb: () => {
+					config.channels.splice(config.channels.indexOf(message.channel.id));
+					config.save();
+					message.reply('Channel removed!');
+				}
+			},
+			{
+				name: 'chlist',
+				admin: true,
+				cb: () => {
+					message.reply('List of CodeBot channels:\n' +
+						config.channels.join('\n'));
+				}
+			},
+			{
+				name: 'promote',
+				admin: true,
+				cb: () => {
+					if (!config.admins.includes(words[2])) {
+						config.admins.push(words[2]);
+						config.save();
+						message.reply('User promoted!');
+					}
+				}
+			},
+			{
+				name: 'demote',
+				admin: true,
+				cb: () => {
+					config.admins.splice(config.admins.indexOf(words[2]));
+					config.save();
+					message.reply('User demoted!');
+				}
+			},
+			{
+				name: 'prset',
+				admin: true,
+				cb: () => {
+					const project = config.projects.find(p => p.name === words[2]);
+					for (const p of config.projects) {
+						if (p !== project) {
+							p.channels.splice(p.channels.indexOf(message.channel.id));
+						}
+					}
+					if (project) {
+						project.channels.push(message.channel.id);
+						message.reply('Project for this channel is set to ' + project.name + '!');
+					} else {
+						message.reply('Project not found, using empty project.');
+					}
+					config.save();
+				}
+			},
+			{
+				name: 'admins',
+				admin: true,
+				cb: () => {
+					message.reply('List of CodeBot admins:\n' +
+						config.admins.join('\n'));
+				}
+			},
+			{
+				name: 'version',
+				cb: () => {
+					configurer('./package.json').load().then(pkg => {
+						message.reply('CodeBot version: ' + pkg.version);
+					});
+				}
+			},
+			{
+				name: 'ahelp',
+				admin: true,
+				cb: () => {
+					message.reply('CodeBot admin help\n' +
+						'!codebot chadd - Adds the current channel to CodeBot\n' +
+						'!codebot chdel - Removes the current channel from CodeBot\n' +
+						'!codebot chlist - Lists channels added to CodeBot\n' +
+						'!codebot promote [id] - Sets user as admin\n' +
+						'!codebot demote [id] - Removes the user from admins\n' +
+						'!codebot admins - Lists all the admins of CodeBot' +
+						'!codebot prset - Sets the project to use for current channel\n' +
+						'!codebot ahelp - Shows this page\n' +
+						'!codebot help - Shows user help page');
+				}
+			},
+			{
+				name: 'help',
+				cb: () => {
+					message.reply('CodeBot help\n' +
+						'Just send me your code and I\'ll format it and check it for any errors!\n' +
+						'!codebot help - Shows this page\n' +
+						'!codebot ahelp - Shows admin help page\n' +
+						'!codebot version - Shows bot version');
+				}
+			}
+		];
+		const command = commands.find(c => c.name === words[1]);
+		if (!command) {
+			message.reply('Command not found');
+		} else if (command.admin && !config.admins.includes(message.author.id)) {
+			message.reply('This command requires admin permissions');
+		} else {
+			command.cb();
+		}
+	}
+
+
+	function processCode(message) {
+		let promise = new Promise(resolve => resolve());
+		const project = config.projects.find(p => p.channels.includes(message.channel.id)) ??
+			config.projects.find(p => p.name === 'empty');
+
+		promise = promise
+			.then(() => fm.cleanDirectory(project.upload))
+			.then(() => fm.cleanDirectory(config.tempDir));
+
+		const attachments = message.attachments.array();
+		if (attachments.length) {
+			const sourceFile = attachments.find(a => a.url.match(/.*\.c(pp)?$/));
+			const sourceArchive = attachments.find(a => a.url.match(/.*\.zip$/));
+			if (sourceFile) {
+				promise = promise
+					.then(() => fm.downloadFile(sourceFile.url, project.upload))
+					.then(() => syntaxChecker.checkProject(project.root));
+			} else if (sourceArchive) {
+				promise = promise
+					.then(() => fm.cleanDirectory(project.upload))
+					.then(() => fm.downloadFile(sourceArchive.url, config.tempDir))
+					.then(() => fm.unArchive(config.tempDir + sourceArchive.name, project.upload))
+					.then(() => syntaxChecker.checkProject(project.root));
+			} else {
+				promise = promise
+					.then(() => new Promise((resolve, reject) => reject('File type not supported')));
+			}
+		} else {
+			promise = promise
+				.then(() => fm.saveToFile(project.upload + 'upload.cpp', message.content))
+				.then(() => syntaxChecker.checkProject(project.root));
+		}
+		return reply(message, project, promise);
+	}
+
+
+	function reply(message, project, promise) {
+		const archivePath = config.tempDir + 'CodeBot.zip';
+		return promise
+			.then(warnings => warnings? fm.saveToFile(project.upload + 'warnings.txt',
+				warnings) : null)
+			.catch(err => err? fm.saveToFile(project.upload + 'errors.txt',
+				err) : null)
+			.then(() => message.content? fm.saveToFile(project.upload + 'message.txt',
+				message.content) : null)
+			.then(() => fm.archive(project.upload, archivePath))
+			.then(() => message.channel.send('<@' + message.author.id + '>,\n' +
+				'See attached file for detailed output.',
+				new Discord.MessageAttachment(fs.createReadStream(archivePath))
+			));
+	}
 });
-
-
-function processCode(message) {
-
-}
