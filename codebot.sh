74867f33462ce134bc3f910556c365ee464d6c39
vze3f372 <joe.hotchkiss@gmail.com>
2021-02-27 09:55:53 +0200

    Add user to the sheel script

--- codebot.sh
+++ codebot.sh
@@ -4,10 +4,8 @@ useradd -u 1500 bot
 
 git clone https://github.com/vze3f372/formatbot.git
 cd formatbot/
-git clone https://github.com/vze3f372/zumoPreBuilt.git
-
-mkdir -p ./projects/zumo/upload ./projects/empty/upload
-cp -r ./zumoPreBuilt/* ./projects/zumo
+mkdir -p ./projects/zumo/upload/ ./projects/empty/upload/
+git clone https://github.com/vze3f372/zumoPreBuilt.git ./projects/zumo/
 rm -rf ./zumoPreBuilt/
 chmod ug+rx ./*
 chmod gu+rwx ./projects/*/upload
